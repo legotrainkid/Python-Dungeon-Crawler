401549b867451dd6a562b74e9a88ab04e80c3eb7
Mason B <legotrainkid@gmail.com>
2020-11-10 21:37:40 -0500

    more work

---
+++ ENTITIES.py
@@ -0,0 +1,61 @@
+import arcade
+
+class Tile(arcade.Sprite):
+    def __init__(self, texture, x, y, is_barrier, pos, SCREENSIZE):
+        super().__init__()
+
+        self.texture = texture
+        
+        self.x = x
+        self.y = y
+        
+        self.is_barrier = is_barrier
+        
+        self.pos = pos
+        
+        self.SCREENSIZE = SCREENSIZE
+
+
+class Player(arcade.Sprite):
+    def __init__(self, health, stamina, damage, SCREENSIZE):
+        super().__init__()
+
+        self.texture = arcade.texture.load_texture("resources/graphics/characters/Player/player.png")
+
+        self.center_x = int(SCREENSIZE[0]/2-17.5)
+        self.center_y = int(SCREENSIZE[1]/2-17.5)
+        self.pos = []
+
+        self.MAX_HEALTH = health
+        self.health = health
+
+        self.sprint = False
+        self.stamina = stamina
+        self.MAX_STAMINA = stamina
+
+        self.NORMAL_DMG = damage
+        self.damage = damage
+
+    def update(self, move):
+        if self.stamina < 1:
+            self.sprint = False
+            self.stamina = 0
+        if self.sprint:
+            if not self.update_frames:
+                self.stamina -= 1
+                self.update_frames = 1
+            else:
+                self.update_frames-=1
+        elif not self.sprint:
+            if not self.update_frames:
+                if self.stamina < self.MAX_STAMINA:
+                    self.stamina +=1
+                    self.update_frames = 5
+            else:
+                self.update_frames-=1
+        if self.update_frames < 0:
+            self.update_frames = 0
+
+class Enemy(arcade.Sprite):
+    def __init__(self, pos, frame, SCREENSIZE):
+        super().__init__()
--- GAME.py
+++ GAME.py
@@ -32,6 +32,8 @@ class Game(arcade.View):
         self.PLAYER_DAMAGE = int(self.config["game"]["player_damage"])
         self.ARROW_SPEED = int(self.config["game"]["arrow_speed"])
         self.ARROW_DAMAGE = int(self.config["game"]["arrow_damage"])
+        self.SCREENSIZE = [int(self.config["init"]["width"]),
+                               int(self.config["init"]["height"])]
 
         self.items = DATA.load_items()
 
@@ -46,15 +48,95 @@ class Game(arcade.View):
         #Get world map
         self.world_map = dg.return_tiles()
 
+        self.TILES = {}
+        load_tiles = ["empty", "floor", "wall"]
+        for tile in load_tiles:
+            self.TILES[tile] = arcade.texture.load_texture("resources/graphics/world/"+tile+".png")
+
+        self.all_sprites = arcade.SpriteList()
+        self.walls = arcade.SpriteList(use_spatial_hash=True)
+        self.tiles = arcade.SpriteList(use_spatial_hash=True)
+        self.enemies = arcade.SpriteList()
+        self.arrows = arcade.SpriteList()
+        self.tiles_list = arcade.SpriteList(use_spatial_hash=True)
+
+        y_v = 0
+        x_v = 0
+        i = 0
+        #Loop trough every tile in map
+        for y in range(len(self.world_map)):
+            for x in self.world_map[y]:
+                #Create tile sprite
+                if x == 0:
+                    new = ENTITIES.Tile(
+                        self.TILES["empty"], x_v, y_v,
+                        True, [i, y], self.SCREENSIZE)
+                elif x == 1:
+                    new = ENTITIES.Tile(
+                        self.TILES["wall"], x_v, y_v,
+                        True, [i, y], self.SCREENSIZE)
+                    self.walls.append(new)
+                elif x == 2:
+                    new = ENTITIES.Tile(
+                        self.TILES["floor"], x_v, y_v,
+                        False, [i, y], self.SCREENSIZE)
+                x_v += 50
+                #add tile to sprite groups
+                self.tiles_list.append(new)
+                self.all_sprites.append(new)
+                i+=1
+            y_v += 50
+            x_v = 0
+            i = 0
+
+        self.player = ENTITIES.Player(30, 500, self.PLAYER_DAMAGE, self.SCREENSIZE)
+
+        self.spawn_enemies(self.NUM_ENEMIES)
+        self.all_sprites.add(self.player)
+
+
     def on_show_view(self):
-        self.setup()
         arcade.set_background_color(arcade.color.WHITE)
+        self.on_draw()
+        self.setup()
 
     def on_draw(self):
         arcade.start_render()
+        arcade.finish_render()
 
     def on_update(self, delta_time):
         if self.paused:
             return
+
+    def spawn_enemies(self, num):
+        for i in range(num):
+            pos = self.spawn_ent()
+            enemy = ENTITIES.Enemy(pos, i, self.SCREENSIZE)
+            self.all_sprites.add(enemy)
+            self.enemies.add(enemy)
+
+    def spawn_ent(self):
+        tiles = self.spawnable_tiles()
+        spawned = False
+        while not spawned:
+            tile = random.choice(tiles)
+            pos = self.tile_pos(tile)
+            spawned = True
+        return pos
+
+    def spawnable_tiles(self):
+        spawnable = []
+        for y in range(len(self.world_map)):
+            for x in range(len(self.world_map[y])):
+                if self.world_map[y][x] == 2:
+                    spawnable.append([x, y])
+        return spawnable
+
+    def tile_pos(self, tile):
+        pos_x = tile[0] * 50
+        pos_y = tile[1] * 50
+        return [pos_x, pos_y]
+
+        
 
                
